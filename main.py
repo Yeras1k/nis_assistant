20652ef3dd6785122e432275dd2b8beefdf5ae43
Yeras1k <yerasylhello@gmail.com>
2022-11-19 22:20:42 +0600

    fff1

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ mycursor = mydb.cursor(buffered=True)
 def start(message):
     mycursor.execute(f"SELECT teleid FROM students WHERE teleid = %s",(message.chat.id,))
     result = mycursor.fetchone()
-    bot.send_message(message.chat.id, f'{result[0]}')
+    bot.send_message(message.chat.id, f'{result}')
     if not result[0]:
         service = telebot.types.ReplyKeyboardMarkup(True, True)
         service.row('student', 'curator')

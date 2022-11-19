e769a997b3bbb46e388616ffb993873efc3f1f3e
Yeras1k <yerasylhello@gmail.com>
2022-11-20 00:25:35 +0600

    ewbtr

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ def select_class(message):
         bot.register_next_step_handler(msg, start)
     else:
         global students
-        mycursor.execute(f"SELECT teleid FROM students WHERE class = %s",(message.text))
+        mycursor.execute(f"SELECT teleid FROM students WHERE class = %s",(message.text,))
         students = mycursor.fetchall()
         service = telebot.types.ReplyKeyboardMarkup(True, True)
         service.row('Отмена')
